48634628bde377fefab80c8a8901e0cb25f59905
brentdeguzman <brent.deguzman@98labs.com>
2024-05-06 10:15:34 +0800

    Add lazy loading

--- server/src/main/java/com/_98labs/exercises/sockets/server/ServerPoemLineLazy.java
+++ server/src/main/java/com/_98labs/exercises/sockets/server/ServerPoemLineLazy.java
@@ -2,34 +2,41 @@ package com._98labs.exercises.sockets.server;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
 import java.io.*;
+import java.util.Map;
 
-public class ServerPoemLine {
-    private static final Logger poemLineLogger = LogManager.getLogger(ServerPoemLine.class);
-    private static String filePath = ServerPoemLine.class.getClassLoader().getResource("Poem.txt").getPath();
+public class ServerPoemLineLazy {
+    private static final Logger poemLineLogger = LogManager.getLogger(ServerPoemLineLazy.class);
+    private static String filePath = ServerPoemLineLazy.class.getClassLoader().getResource("Poem.txt").getPath();
     //getPath() -> convert URL object to string
     //getResources() -> returns a URL object representing the location of the resource
     //getClassLoader() -> loads classes from the target folder
     private static BufferedReader clientInput;
     private static int lineNumber;
     private static int defaultValue;
-    private static final List<String> poemLines = new ArrayList<>();//dynamic array for storing the elements
+    private static Map<Integer, String> poemLines = new HashMap<>();
+    public static String lazyGetPoemLine(int lineNumber) {
 
-    private static void loadPoem() {
         try (BufferedReader fileReader = new BufferedReader(new FileReader(filePath))) {
+            int currentLineNumber = 0;
             String line;
             while ((line = fileReader.readLine()) != null) {
-                poemLines.add(line);
-            }//line is successfully read and is not null, it is added to list
-            poemLineLogger.info("Poem file loaded into memory.");
+                currentLineNumber++;
+                if (currentLineNumber == lineNumber) {
+                    poemLineLogger.info("Line " + lineNumber + " cached.");
+                    poemLines.put(lineNumber,line);
+                    System.out.println(poemLines);
+                    return line;
+                }
+            }
+            poemLineLogger.warn("Line " + lineNumber + " not found in the poem file.");
         } catch (IOException e) {
             poemLineLogger.error("Error reading poem file: " + e.getMessage());
         }
+        return null; //lineNumber exceeds
     }
-    static {loadPoem();}//initialize the loadPoem into memory
 
     public static int handleLineNumberFromClient() throws IOException {
         clientInput = new BufferedReader(new InputStreamReader(Server.clientSocket.getInputStream()));
@@ -51,21 +58,5 @@ public class ServerPoemLine {
             poemLineLogger.warn("The input is invalid.");
             return terminateValue; //Invalid input (not an integer)
         }
-    }
-
-//    public void setFilePath(String filePath) {
-//        this.filePath = filePath;
-//    }
-    public static String readPoem(int lineNumber) {
-        if (lineNumber < 1) {
-            poemLineLogger.warn("Poem line does not exist.");
-            return null;
-        } else if (lineNumber > poemLines.size()) {//number of element from list
-            poemLineLogger.warn("Requested poem line exceed.");
-            return null;
-        }
-        String line = poemLines.get(lineNumber - 1);//adjusted for zero-based index array
-        poemLineLogger.info(line);
-        return line;
     }
 }
